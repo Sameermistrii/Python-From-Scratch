82229591c4d069b379ba7face95e421f56fd57ff
Sameer Mistri <sameermistry14@gmail.com>
2026-08-08 15:06:07 +0530

    Lists

---
+++ 11_Lists/01_lists.py
@@ -0,0 +1,7 @@
+marks = [85, 92, 78, 96, 88]
+mixed = ["Hello", 42, 3.14, True]
+
+print(marks[0])
+print(marks[2:4])
+print(mixed[0])
+print(mixed[4]) #index error ku ki 4 number list ma hai he nhi 
