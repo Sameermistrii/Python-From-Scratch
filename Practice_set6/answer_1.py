c77678aa7787ae0d53d4b07073b23799b03da0fc
Sameer Mistri <sameermistry14@gmail.com>
2026-08-12 10:49:26 +0530

    P

---
+++ Practice_set6/answer_1.py
@@ -0,0 +1,9 @@
+"""Create a class  Car  with a method  drive()  that prints  "Car is moving" .
+Create an object of  Car  and call  drive() ."""
+ 
+class Car:
+    def drive(self):
+        print("Car is moving")
+        
+car1 = Car()
+car1.drive()
